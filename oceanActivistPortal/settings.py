625c7d6c10fb000f17fe1ddae251adc51d3bdff1
mrjasonwalton00 <jpwalton00@gmail.com>
2023-04-25 19:04:32 -0400

    new

--- oceanActivistPortal/settings.py
+++ oceanActivistPortal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^f=_8kt#q0mip509=kf3wg#$uiju*r!krub064*vi59)mg!m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

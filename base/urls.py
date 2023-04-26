e7fa37d2e83aa286756acc30f475a967bca41257
mrjasonwalton00 <jpwalton00@gmail.com>
2023-04-26 09:53:05 -0400

    register

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,18 @@ urlpatterns = [
     #base urls
     path('', views.homePage, name='homePage'),
     path('registerPage/', views.registerPage, name='registerPage'),
+
+    # Register Buddy urls
+    path('registerWhale/', views.registerWhale, name='registerWhale'),
+    path('registerTurtle/', views.registerTurtle, name='registerTurtle'),
+    path('registerSeal/', views.registerSeal, name='registerSeal'),
+    path('registerDolphin/', views.registerDolphin, name='registerDolphin'),
+    path('registerSeagull/', views.registerSeagull, name='registerSeagull'),
+
+    # Portal urls
+
+
+
+
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 
 
-#userReg Views
+# Home Page Views
 def homePage(request):
     return render(request, 'base/homePage.html')
 
 def registerPage(request):
-    return render(request, 'base/registerPage.html' )
+    return render(request, 'base/userReg/registerPage.html' )
+
+# Register Buddy Views
+
+def registerDolphin(request):
+    return render(request, 'base/userReg/registerDolphin.html' )
+
+def registerTurtle(request):
+    return render(request, 'base/userReg/registerTurtle.html' )
+
+def registerWhale(request):
+    return render(request, 'base/userReg/registerWhale.html' )
+
+def registerSeal(request):
+    return render(request, 'base/userReg/registerSeal.html' )
+
+def registerSeagull(request):
+    return render(request, 'base/userReg/registerSeagull.html' )
+
+
+# End of Register Buddy Views
 
 

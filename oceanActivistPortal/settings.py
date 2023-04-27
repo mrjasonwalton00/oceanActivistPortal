ab69768e9eb0a2eb294a70b84fe5410694472b73
mrjasonwalton00 <jpwalton00@gmail.com>
2023-04-27 01:58:10 -0400

    debug

--- oceanActivistPortal/settings.py
+++ oceanActivistPortal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^f=_8kt#q0mip509=kf3wg#$uiju*r!krub064*vi59)mg!m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

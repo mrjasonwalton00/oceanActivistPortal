35a9dc56e9d8a75b664ac480a50c42847304564d
mrjasonwalton00 <jpwalton00@gmail.com>
2023-05-04 10:19:42 -0400

    game

--- decompress_brotli.py
+++ decompress_brotli.py
@@ -12,3 +12,43 @@ decompressed_data = brotli.decompress(compressed_data)
 with open(r'C:\Users\demo\Desktop\oceanActivistPortal\static\game\Build\game.framework.js', 'wb') as file_out:
     # Write the decompressed data to the new file
     file_out.write(decompressed_data)
+
+
+
+
+
+
+
+
+# Open the Brotli-encoded file in binary mode
+with open(r'C:\Users\demo\Desktop\oceanActivistPortal\static\game\Build\game.data.br', 'rb') as file_in:
+    # Read the Brotli-encoded data from the file
+    compressed_data = file_in.read()
+
+# Decompress the Brotli-encoded data using the brotli.decompress() function
+decompressed_data = brotli.decompress(compressed_data)
+
+# Open a new file for writing the decompressed data
+with open(r'C:\Users\demo\Desktop\oceanActivistPortal\static\game\Build\game.data', 'wb') as file_out:
+    # Write the decompressed data to the new file
+    file_out.write(decompressed_data)
+
+
+
+
+
+
+
+
+# Open the Brotli-encoded file in binary mode
+with open(r'C:\Users\demo\Desktop\oceanActivistPortal\static\game\Build\game.wasm.br', 'rb') as file_in:
+    # Read the Brotli-encoded data from the file
+    compressed_data = file_in.read()
+
+# Decompress the Brotli-encoded data using the brotli.decompress() function
+decompressed_data = brotli.decompress(compressed_data)
+
+# Open a new file for writing the decompressed data
+with open(r'C:\Users\demo\Desktop\oceanActivistPortal\static\game\Build\game.wasm', 'wb') as file_out:
+    # Write the decompressed data to the new file
+    file_out.write(decompressed_data)
